25ce7c6fbf14e0914dc88565cb2df25bda4dabde
Alex Borisov <alexb88gh@gmail.com>
2026-04-03 17:11:18 +0300

    added routing, added project structure

---
+++ src/app/layouts/main/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export const MainLayout = () => {
+  return (
+    <div className="min-h-screen">
+      <header className="border-b p-4">Demo Store</header>
+
+      <main className="p-4">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
